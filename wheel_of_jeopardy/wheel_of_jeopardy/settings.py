a1236c3c4e147187d5d41c88850a79b2e2f88359
Josh <jmcgiff94@gmail.com>
2019-07-14 22:38:16 -0400

    added wheel_of_jeopardy to installed apps in settings

--- wheel_of_jeopardy/wheel_of_jeopardy/settings.py
+++ wheel_of_jeopardy/wheel_of_jeopardy/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -37,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'wheel_of_jeopardy'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'wheel_of_jeopardy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['wheel_of_jeopardy/templates/',],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
