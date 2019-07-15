438d796d355a80beed7710fde5dc0059ff31ec1e
huntercaskey <hunterrcaskey@gmail.com>
2019-07-14 22:13:26 -0400

    Fixing gameLogic import, adding a gitignore file to remove tracking of .pyc files.

--- wheel_of_jeopardy/wheel_of_jeopardy/urls.py
+++ wheel_of_jeopardy/wheel_of_jeopardy/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 admin.autodiscover()
-import gameLogic
+from wheel_of_jeopardy import gameLogic
 
 
 urlpatterns = [
