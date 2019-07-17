29386f9c9e62a7213c8f08372c42c2452f3bb8bc
dgrieshop <danielgrieshop@gmail.com>
2019-07-16 21:48:54 -0500

    model update for each question

--- wheel_of_jeopardy/wheel_of_jeopardy/models.py
+++ wheel_of_jeopardy/wheel_of_jeopardy/models.py
@@ -8,23 +8,34 @@ class Category(models.Model):
     category_title = models.CharField(max_length=200)
 
 
-class Question(models.Model):
-    '''
-    Stores a single question and ties it to a category. Many-to-one relationship with Category.
-
-    GameSession used as optional secondary foreign key, allows a list of already asked questions to be maintained
-    during the game.
-    '''
-    question_text = models.CharField(max_length=200)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    game_session = models.ForeignKey('GameSession', null=True, blank=True, on_delete=models.SET_NULL)
-
 class Answer(models.Model):
     '''
     Stores the answer to a question. One-to-one relationship with Question.
     '''
     answer_text = models.CharField(max_length=200)
-    question = models.ForeignKey('Question', on_delete=models.CASCADE)
+
+
+class Question(models.Model):
+    '''
+    Stores a single question and ties it to:
+      category -> Many-to-one relationship with Category.
+      answer -> One-to-One relationship with Answer
+
+    GameSession used as optional secondary foreign key, allows a list of already asked questions to be maintained
+    during the game.
+    '''
+    question_text = models.CharField(max_length=400)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+    point_value = models.IntegerField
+    asked = models.BooleanField(default=False)
+
+    ROUND_NUMBER = (
+            ('1', 'One'),
+            ('2', 'Two'),
+        )
+    round_number = models.CharField(max_length=1, choices=ROUND_NUMBER)
+    game_session = models.ForeignKey('GameSession', null=True, blank=True, on_delete=models.SET_NULL)
 
 
 class User(models.Model):
@@ -46,6 +57,6 @@ class GameSession(models.Model):
     #TODO: discuss desire on delete behavior and proper form of related_names
     #current_user = models.CharField(max_length=30) Should this be handles in the db?
     turns_remaining = models.IntegerField
-    User1_profile = models.ForeignKey(User, on_delete=models.CASCADE, related_name='one')
-    User2_profile = models.ForeignKey(User, on_delete=models.CASCADE, related_name="two")
+    User1_profile = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user1')
+    User2_profile = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user2")
 
